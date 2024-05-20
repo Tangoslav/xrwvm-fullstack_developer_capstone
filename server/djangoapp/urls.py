051847263a8efa6e39e072c794af53ad63380bf0
Your Name <you@example.com>
2024-05-20 14:08:22 -0400

    dealers and reviews

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,22 +6,31 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.logout_request, name='logout'),
+    # Path for registration
     path(route='register', view=views.registration, name='register'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    
+    # Path for login
+    path(route='login', view=views.login_user, name='login'),
+    
+    # Path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
+    
+    # Path for getting cars
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+    
+    # Path for getting dealerships
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    
+    # Path for getting dealerships by state
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'), 
+    
+    # Path for getting dealer details by dealer ID
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    
+    # Path for getting dealer reviews by dealer ID
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
+    
+    # Path for adding a review
     path(route='add_review', view=views.add_review, name='add_review'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
