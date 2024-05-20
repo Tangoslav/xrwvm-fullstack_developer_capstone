8ba139993d76873ec06f558bf983126c0b3c0565
Your Name <you@example.com>
2024-05-20 13:34:21 -0400

    finished models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,60 +1,37 @@
-# Uncomment the required imports before adding the code
-
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
-
 from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .models import CarMake, CarModel
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-# Create your views here.
-
-# Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
-    # Get username and password from request.POST dictionary
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
     if user is not None:
-        # If user is valid, call login method to login current user
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
-# Create a `logout_request` view to handle sign out request
 @csrf_exempt
 def logout_request(request):
-    # Get the username from the session
     username = request.user.username
-    # Log out the user
     logout(request)
-    # Return a JSON response with the username
     data = {"userName": username}
     return JsonResponse(data)
 
-# Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
 @csrf_exempt
 def registration(request):
     context = {}
-
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -62,40 +39,75 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
     try:
-        # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
-        # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
-
-    # If it is a new user
+    except User.DoesNotExist:
+        logger.debug(f"{username} is a new user")
     if not username_exist:
-        # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
-        # Login the user and redirect to list page
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
-        data = {"userName":username,"error":"Already Registered"}
+    else:
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
+@csrf_exempt
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    if count == 0:
+        initiate()
+        logger.debug("No CarMakes found, initializing data.")
+    else:
+        logger.debug(f"Found {count} CarMakes in the database.")
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+    car_models = CarModel.objects.select_related('car_make')
+    cars = [{"CarModel": car_model.name, "CarMake": car_model.car_make.name} for car_model in car_models]
+    
+    if not cars:
+        logger.debug("No cars found in the database.")
+    else:
+        logger.debug(f"Found {len(cars)} cars in the database.")
 
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+    return JsonResponse({"CarModels": cars})
 
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+def initiate():
+    car_make_data = [
+        {"name": "NISSAN", "description": "Great cars. Japanese technology"},
+        {"name": "Mercedes", "description": "Great cars. German technology"},
+        {"name": "Audi", "description": "Great cars. German technology"},
+        {"name": "Kia", "description": "Great cars. Korean technology"},
+        {"name": "Toyota", "description": "Great cars. Japanese technology"},
+    ]
+
+    car_make_instances = []
+    for data in car_make_data:
+        car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+
+    car_model_data = [
+        {"name": "Pathfinder", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
+        {"name": "Qashqai", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
+        {"name": "XTRAIL", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
+        {"name": "A-Class", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
+        {"name": "C-Class", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
+        {"name": "E-Class", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
+        {"name": "A4", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
+        {"name": "A5", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
+        {"name": "A6", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
+        {"name": "Sorrento", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[3]},
+        {"name": "Carnival", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[3]},
+        {"name": "Cerato", "car_type": "Sedan", "year": 2023, "car_make": car_make_instances[3]},
+        {"name": "Corolla", "car_type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
+        {"name": "Camry", "car_type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
+        {"name": "Kluger", "car_type": "SUV", "year": 2023, "car_make": car_make_instances[4]},
+    ]
+
+    for data in car_model_data:
+        CarModel.objects.create(
+            name=data['name'],
+            car_make=data['car_make'],
+            car_type=data['car_type'],
+            year=data['year'],
+            dealer_id=1  # Set a default dealer_id or update as per your data
+        )
